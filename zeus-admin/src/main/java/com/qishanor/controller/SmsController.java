5b85aff7b2b5e2281bb82dba45aca6db93bdfad1
Joe <956459902@163.com>
2025-06-18 20:51:14 +0800

    12

--- zeus-admin/src/main/java/com/qishanor/controller/SmsController.java
+++ zeus-admin/src/main/java/com/qishanor/controller/SmsController.java
@@ -21,23 +21,7 @@ public class SmsController {
     @RequestMapping("/sms")
     public Object sendSms(){
 
-        SmsConfig smsConfig=new SmsConfig();
-        smsConfig.setSupplier("ali");
-        smsConfig.setRegionId("cn-qingdao");
-        smsConfig.setAccessKey("LTAI5tGdfdQNma1oYRVPzmv7");
-        smsConfig.setSecretKey("UBSjsDeVd9JsJkrJGFw0j7UGUs7Myz");
-        smsConfig.setSignName("启善智能");
 
-//        SmsTemplate smsTemplate = new SmsAliTemplate(smsConfig);
-
-        String phone = "13345092258";
-        String templateCode = "SMS_278275295";
-        HashMap<String,String> map=new HashMap<>();
-        map.put("code","1234");
-        String templateParam = JSONUtil.toJsonStr(map);
-
-        SmsResult smsResult = smsTemplate.send(null,phone,templateCode,map);
-        Console.log("sendResponse:" + smsResult);
         return R.ok();
     }
 }
