f0cb3a6789c52e77682403ef2bfd9927e039b434
lduzhenlin <956459902@163.com>
2025-12-04 20:28:31 +0800

    补充 全局异常捕获和后端数据校验

--- zeus-admin/src/main/java/com/qishanor/entity/req/CategoryRequest.java
+++ zeus-admin/src/main/java/com/qishanor/entity/req/CategoryRequest.java
@@ -1,12 +1,13 @@
 package com.qishanor.entity.req;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CategoryRequest {
 
-    @NotNull(message = "分类名不能为空")
+    @NotBlank(message = "分类名不能为空")
     private String name;
     /**
      * 排序值，数值越小越靠前
--- zeus-admin/src/main/java/com/qishanor/entity/req/LinkRequest.java
+++ zeus-admin/src/main/java/com/qishanor/entity/req/LinkRequest.java
@@ -1,13 +1,17 @@
 package com.qishanor.entity.req;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LinkRequest {
 
+    @NotBlank(message = "标题不能为空")
     private String title;
+    @NotBlank(message = "地址不能为空")
     private String url;
     private String description;
     private String icon;
+    @NotBlank(message = "分类不能为空")
     private String categoryId;
 }
